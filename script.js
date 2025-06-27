89a02ab4a90e6ca4ceac5d774b07a45a527552df
Bendersnatch <bazhenovo89@gmail.com>
2025-06-27 22:10:07 +0200

    aggiornamenti

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-document.querySelectorAll("a").forEach((link) => {
-  if (link.href && !link.href.startsWith("#")) {
-    link.addEventListener("click", function (e) {
+/*document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("a").forEach((link) => {
+    const rawHref = link.getAttribute("href");
+
+    if (rawHref && !rawHref.startsWith("#")) {
+      link.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.body.classList.add("glitch-transition");
+
+        const destination = this.href;
+
+        setTimeout(() => {
+          window.location.href = destination;
+        }, 600);
+      });
+    }
+  });
+});*/
+document.querySelectorAll("a, .nav-button").forEach((el) => {
+  const destination = el.tagName === "A" ? el.href : el.dataset.href;
+
+  if (destination && !destination.startsWith("#")) {
+    el.addEventListener("click", function (e) {
       e.preventDefault();
       document.body.classList.add("glitch-transition");
-
-      const destination = this.href;
 
       setTimeout(() => {
         window.location.href = destination;
@@ -12,3 +30,4 @@ document.querySelectorAll("a").forEach((link) => {
     });
   }
 });
+
